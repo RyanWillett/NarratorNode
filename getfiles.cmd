afd52f6e2f219e8267fe93ee9bf3f583e383f2e5
euromkay <euromkay@gmail.com>
2016-07-21 08:26:34 -0700

    added stateobject to copy pull

--- getfiles.cmd
+++ getfiles.cmd
@@ -1,6 +1,7 @@
 xcopy /s/y C:\Users\Michael\Documents\GitHub\TheNarrator\*.* src\shared
 copy /y C:\Users\Michael\Documents\GitHub\NarratorAndroid\app\src\main\java\android\texting\TextInput.java src\android\texting
 copy /y C:\Users\Michael\Documents\GitHub\NarratorAndroid\app\src\main\java\android\texting\TextHandler.java src\android\texting
+copy /y C:\Users\Michael\Documents\GitHub\NarratorAndroid\app\src\main\java\android\texting\StateObject.java src\android\texting
 xcopy /s/y C:\Users\Michael\workspace\NarratorNode\src\*.* src\
 
 del /s *.class
